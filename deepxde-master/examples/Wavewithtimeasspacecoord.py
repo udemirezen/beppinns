ba08a05b8d99ffa5150acf98d0bc8b87bc281fc3
jort99b <55081710+jort99b@users.noreply.github.com>
2020-06-05 15:00:26 +0200

    5 June

--- deepxde-master/examples/Wavewithtimeasspacecoord.py
+++ deepxde-master/examples/Wavewithtimeasspacecoord.py
@@ -131,7 +131,7 @@ def space_boundary(x, on_boundary):
     return on_boundary and (np.isclose(x[0], -1) or np.isclose(x[0], 1))
 
 def time_boundary(x, on_boundary):
-    return on_boundary and np.isclose(x[1], 0)
+    return on_boundary and np.isclose(x[1], 0) and not (np.isclose(x[0], -1) or np.isclose(x[0], 1))
 
 geomtime = dde.geometry.Rectangle([-1, 0], [1, 2.49])
 
@@ -139,7 +139,7 @@ bc = dde.DirichletBC(geomtime, lambda x: np.zeros((len(x), 1)), space_boundary)
 ic1 = dde.DirichletBC(geomtime, lambda x: -np.sin(np.pi * x[:, 0:1]), time_boundary)
 ic2=dde.NeumannBC(geomtime, lambda x: np.zeros((len(x),1)), time_boundary)
 
-data = dde.data.PDE(geomtime, 1, pde, [bc, ic1, ic2], num_domain=5080, num_boundary=320)
+data = dde.data.PDE(geomtime, 1, pde, [bc, ic1, ic2], num_domain=5080, num_boundary=640)
 net = dde.maps.FNN([2] + [20] * 3 + [1], "tanh", "Glorot normal")
 model = dde.Model(data, net)
 
